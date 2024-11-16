e4e335e3cd29ac4ddf77e27cc4ed2b88d915c968
Kevin Müller <kevin.mueller@tuta.com>
2024-11-16 14:07:41 +0100

    initial setup of join backend

---
+++ backend_app/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+# Create your models here.
+class Tasks(models.Model):
+    pass
+
+
+class Summary(models.Model):
+    pass
+
+
+class Contacts(models.Model):
+    pass
