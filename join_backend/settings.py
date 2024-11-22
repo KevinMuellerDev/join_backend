10ef7539f2543ed9476eb087a35bd9d20f3b8cfe
Kevin Mueller <kevin.mueller@tuta.com>
2024-11-22 07:40:45 +0100

    setup settings for localhost fetch permissions

--- join_backend/settings.py
+++ join_backend/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-zrm&pbo-1km90g36qb=t(b&ck-ishcjf-m39lrq#g+glk8mwc!
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
     'backend_app',
     'user_auth_app',
     'rest_framework',
@@ -47,6 +48,7 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
+    "corsheaders.middleware.CorsMiddleware",
     #'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
@@ -69,6 +71,13 @@ TEMPLATES = [
             ],
         },
     },
+]
+
+CORS_ALLOWED_ORIGINS = [
+    "https://example.com",
+    "https://sub.example.com",
+    "http://localhost:5500",
+    "http://127.0.0.1:5500",
 ]
 
 WSGI_APPLICATION = 'join_backend.wsgi.application'
