ef708a9587c090d9061ab4fd79f815af49d8b0b2
Kevin Mueller <kevin.mueller@tuta.com>
2024-11-27 10:44:50 +0100

    added id to contacts in tasks model

--- backend_app/api/serializers.py
+++ backend_app/api/serializers.py
@@ -93,7 +93,7 @@ class TaskSerializer(serializers.ModelSerializer):
     def to_representation(self, instance):
         representation = super().to_representation(instance)
         representation['assigned'] = [
-            {'name': contact.name, 'initials': contact.initials, 'circle_color': contact.circle_color} for contact in instance.assigned.all()
+            {'id':contact.id, 'name': contact.name, 'initials': contact.initials, 'circle_color': contact.circle_color} for contact in instance.assigned.all()
         ]
 
         representation['subtasks'] = [
